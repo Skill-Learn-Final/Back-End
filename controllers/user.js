c529cbb6c747dfdac9206bd47b7111933ae31686
Kaleb-Besufikad <se.kaleb.besufikad@gmail.com>
2023-06-14 11:12:15 +0300

    added profile update

--- controllers/user.js
+++ controllers/user.js
@@ -4,278 +4,367 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const { sendPasswordResetEmail } = require("../middleware/sendEmail");
 const { StatusCodes } = require("http-status-codes");
-
+const util = require("util");
+const pbkdf2Async = util.promisify(crypto.pbkdf2);
 const createUser = (req, res, next) => {
-  try {
-    let { firstName, lastName, email, role } = req.body;
-    let password = crypto.randomBytes(16).toString("hex");
+    try {
+        let { firstName, lastName, email, role } = req.body;
+        let password = crypto.randomBytes(16).toString("hex");
 
-    var salt = crypto.randomBytes(16);
-    crypto.pbkdf2(
-      password,
-      salt,
-      310000,
-      32,
-      "sha256",
-      async function (err, passwordHash) {
-        if (err) {
-          return next(err);
+        var salt = crypto.randomBytes(16);
+        crypto.pbkdf2(
+            password,
+            salt,
+            310000,
+            32,
+            "sha256",
+            async function(err, passwordHash) {
+                if (err) {
+                    return next(err);
+                }
+                try {
+                    const createdUser = await db.User.create({
+                        // username: req.body.username,
+                        email,
+                        firstName,
+                        lastName,
+                        passwordHash,
+                        passwordSalt: salt,
+                        roles: [{
+                            role,
+                        }, ],
+                    }, { include: ["roles"] });
+
+                    const secret = process.env.JWT_SECRET + createdUser.passwordHash;
+                    const payload = {
+                        email: createdUser.email,
+                        id: createdUser.id,
+                    };
+                    const token = jwt.sign(payload, secret, { expiresIn: "2m" });
+                    const link = `http://localhost:3000/reset_password/${createdUser.id}/${token}`;
+                    // res.send(link);
+                    sendPasswordResetEmail(email, createdUser.firstName, link)
+                        .then((response) => res.send(response.message))
+                        .catch((error) => console.log(error.message));
+                    res.status(200).send("Email Sent");
+                } catch (err) {
+                    console.log(err);
+                    return res.status(409).json(err);
+                }
+            }
+        );
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error);
+    }
+};
+
+const getUserList = async(req, res, next) => {
+    try {
+        const { role } = req.query;
+
+        const users = await db.User.findAll({
+            attributes: { exclude: ["passwordHash", "passwordSalt"] },
+            include: [{
+                model: db.UserRole,
+                as: "roles",
+                attributes: ["role"],
+                where: role ? { role } : {},
+            }, ],
+        });
+        res.status(StatusCodes.OK).json(users);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+};
+
+const deleteUser = async(req, res, next) => {
+    try {
+        const { id } = req.params;
+        const user = await db.User.findOne({ where: { id } });
+        if (!user) {
+            return res.status(StatusCodes.NOT_FOUND).json({
+                message: "User not found",
+            });
         }
-        try {
-          const createdUser = await db.User.create(
-            {
-              // username: req.body.username,
-              email,
-              firstName,
-              lastName,
-              passwordHash,
-              passwordSalt: salt,
-              roles: [
-                {
-                  role,
-                },
-              ],
-            },
-            { include: ["roles"] }
-          );
+        await user.destroy();
+        res.status(StatusCodes.OK).json({
+            message: "User deleted successfully",
+        });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+};
 
-          const secret = process.env.JWT_SECRET + createdUser.passwordHash;
-          const payload = {
-            email: createdUser.email,
-            id: createdUser.id,
-          };
-          const token = jwt.sign(payload, secret, { expiresIn: "2m" });
-          const link = `http://localhost:3000/reset_password/${createdUser.id}/${token}`;
-          // res.send(link);
-          sendPasswordResetEmail(email, createdUser.firstName, link)
-            .then((response) => res.send(response.message))
-            .catch((error) => console.log(error.message));
-          res.status(200).send("Email Sent");
-        } catch (err) {
-          console.log(err);
-          return res.status(409).json(err);
+const profileVerificationRequest = async(req, res, next) => {
+    try {
+        const { professionalTitle, address } = req.body;
+        const { governmentId, proofDocument } = req.files;
+
+        const request = await db.ProfileVerification.create({
+            creatorId: "639ae82a-a775-49e0-8b26-9e247d73628f",
+            professionalTitle,
+            address,
+            governmentIdLink: `${req.protocol}://${req.hostname}:8080/uploads/${governmentId[0].filename}`,
+            proofDocumentLink: `${req.protocol}://${req.hostname}:8080/uploads/${proofDocument[0].filename}`,
+        });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+};
+
+const getProfileVerificationRequest = async(req, res, next) => {
+    try {
+        const { id } = req.params;
+        const request = await db.ProfileVerification.findOne({
+            where: { id },
+            include: [{
+                model: db.User,
+                as: "creator",
+                attributes: ["firstName", "lastName", "email"],
+            }, ],
+        });
+        if (!request) {
+            return res.status(StatusCodes.NOT_FOUND).json({
+                message: "Request not found",
+            });
         }
-      }
-    );
-  } catch (error) {
-    console.log(error);
-    res.status(400).send(error);
-  }
+        res.status(StatusCodes.OK).json(request);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
 };
 
-const getUserList = async (req, res, next) => {
-  try {
-    const { role } = req.query;
+const getProfileVerificationRequestByUser = async(req, res, next) => {
+    try {
+        const { creatorId } = req.query;
 
-    const users = await db.User.findAll({
-      attributes: { exclude: ["passwordHash", "passwordSalt"] },
-      include: [
-        {
-          model: db.UserRole,
-          as: "roles",
-          attributes: ["role"],
-          where: role ? { role } : {},
-        },
-      ],
-    });
-    res.status(StatusCodes.OK).json(users);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+        if (!creatorId) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                message: "creatorId is required",
+            });
+        }
+
+        const request = await db.ProfileVerification.findOne({
+            where: { creatorId },
+            include: [{
+                model: db.User,
+                as: "creator",
+                attributes: ["firstName", "lastName", "email"],
+            }, ],
+        });
+
+        res.status(StatusCodes.OK).json(request);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
 };
 
-const deleteUser = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const user = await db.User.findOne({ where: { id } });
-    if (!user) {
-      return res.status(StatusCodes.NOT_FOUND).json({
-        message: "User not found",
-      });
+const approveProfileVerificationRequest = async(req, res, next) => {
+    try {
+        const { id } = req.params;
+        const request = await db.ProfileVerification.findOne({
+            where: { id },
+        });
+        if (!request) {
+            return res.status(StatusCodes.NOT_FOUND).json({
+                message: "Request not found",
+            });
+        }
+
+        if (request.isReviewed) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                message: "Request already reviewed",
+            });
+        }
+
+        request.isApproved = true;
+        request.isReviewed = true;
+        await request.save();
+        res.status(StatusCodes.OK).json({
+            message: "Request approved successfully",
+        });
+    } catch (error) {
+        console.log(error);
+        next(error);
     }
-    await user.destroy();
-    res.status(StatusCodes.OK).json({
-      message: "User deleted successfully",
-    });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
 };
 
-const profileVerificationRequest = async (req, res, next) => {
-  try {
-    const { professionalTitle, address } = req.body;
-    const { governmentId, proofDocument } = req.files;
+const rejectProfileVerificationRequest = async(req, res, next) => {
+    try {
+        const { id } = req.params;
+        const request = await db.ProfileVerification.findOne({
+            where: { id },
+        });
+        if (!request) {
+            return res.status(StatusCodes.NOT_FOUND).json({
+                message: "Request not found",
+            });
+        }
 
-    const request = await db.ProfileVerification.create({
-      creatorId: "639ae82a-a775-49e0-8b26-9e247d73628f",
-      professionalTitle,
-      address,
-      governmentIdLink: `${req.protocol}://${req.hostname}:8080/uploads/${governmentId[0].filename}`,
-      proofDocumentLink: `${req.protocol}://${req.hostname}:8080/uploads/${proofDocument[0].filename}`,
-    });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+        if (request.isReviewed) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                message: "Request already reviewed",
+            });
+        }
+
+        request.isApproved = false;
+        request.isReviewed = true;
+        await request.save();
+
+        res.status(StatusCodes.OK).json({
+            message: "Request rejected successfully",
+        });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
 };
 
-const getProfileVerificationRequest = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const request = await db.ProfileVerification.findOne({
-      where: { id },
-      include: [
-        {
-          model: db.User,
-          as: "creator",
-          attributes: ["firstName", "lastName", "email"],
-        },
-      ],
-    });
-    if (!request) {
-      return res.status(StatusCodes.NOT_FOUND).json({
-        message: "Request not found",
-      });
+const getProfileVerificationRequestList = async(req, res, next) => {
+    try {
+        let filter = {};
+        const { status } = req.query;
+
+        if (status === "approved") {
+            filter = { isApproved: true };
+        } else if (status === "rejected") {
+            filter = { isApproved: false, isReviewed: true };
+        } else if (status === "pending") {
+            filter = { isReviewed: false };
+        } else {
+            filter = {};
+        }
+
+        const requests = await db.ProfileVerification.findAll({
+            where: filter,
+            include: [{
+                model: db.User,
+                as: "creator",
+                attributes: ["firstName", "lastName", "email"],
+            }, ],
+        });
+        res.status(StatusCodes.OK).json(requests);
+    } catch (error) {
+        console.log(error);
+        next(error);
     }
-    res.status(StatusCodes.OK).json(request);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
 };
 
-const getProfileVerificationRequestByUser = async (req, res, next) => {
-  try {
-    const { creatorId } = req.query;
+// get user info
 
-    if (!creatorId) {
-      return res.status(StatusCodes.BAD_REQUEST).json({
-        message: "creatorId is required",
-      });
+const getUserInfo = async(req, res, next) => {
+    try {
+        const id = req.params.id;
+        const user = await db.User.findOne({
+            where: { id: id },
+        });
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        const userInfo = {
+            email: user.email,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            profilePicture: user.profilePicture,
+            balance: user.balance,
+            passwordHash: user.passwordHash,
+            emailConfirmed: user.emailConfirmed,
+        };
+        res.status(200).json(userInfo);
+    } catch (error) {
+        next(error);
     }
-
-    const request = await db.ProfileVerification.findOne({
-      where: { creatorId },
-      include: [
-        {
-          model: db.User,
-          as: "creator",
-          attributes: ["firstName", "lastName", "email"],
-        },
-      ],
-    });
-
-    res.status(StatusCodes.OK).json(request);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
 };
 
-const approveProfileVerificationRequest = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const request = await db.ProfileVerification.findOne({
-      where: { id },
-    });
-    if (!request) {
-      return res.status(StatusCodes.NOT_FOUND).json({
-        message: "Request not found",
-      });
-    }
+// update fields
+const updateUser = async(req, res, next) => {
+    try {
+        const id = req.params.id;
+        const possibleUser = await db.User.findOne({ where: { id: id } });
+        const { firstName, lastName, profilePicture } = req.body;
+        if (req.file) {
+            possibleUser.profilePicture = `${req.protocol}://${req.hostname}:8080/uploads/${req.file.filename}`;
+        }
+        possibleUser.firstName = firstName;
+        possibleUser.lastName = lastName;
 
-    if (request.isReviewed) {
-      return res.status(StatusCodes.BAD_REQUEST).json({
-        message: "Request already reviewed",
-      });
+        await possibleUser.save();
+        res
+            .status(StatusCodes.OK)
+            .json({ success: true, message: "User fields updated successfully." });
+    } catch (error) {
+        next(error);
     }
-
-    request.isApproved = true;
-    request.isReviewed = true;
-    await request.save();
-    res.status(StatusCodes.OK).json({
-      message: "Request approved successfully",
-    });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
 };
 
-const rejectProfileVerificationRequest = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const request = await db.ProfileVerification.findOne({
-      where: { id },
+// password
+const changePassword = async(req, res, next) => {
+    const { oldPassword, newPassword } = req.body;
+    const possibleUser = await db.User.findOne({
+        where: { id: req.params.id },
     });
-    if (!request) {
-      return res.status(StatusCodes.NOT_FOUND).json({
-        message: "Request not found",
-      });
+
+    if (!possibleUser) {
+        return res.status(404).json({ error: "User not found" });
     }
 
-    if (request.isReviewed) {
-      return res.status(StatusCodes.BAD_REQUEST).json({
-        message: "Request already reviewed",
-      });
+    const secret = process.env.JWT_SECRET + possibleUser.passwordHash;
+
+    try {
+        const salt = crypto.randomBytes(16);
+        const hashedPassword = await pbkdf2Async(
+            oldPassword,
+            salt,
+            310000,
+            32,
+            "sha256"
+        );
+
+        if (!crypto.timingSafeEqual(possibleUser.passwordHash, hashedPassword)) {
+            return res.status(401).json({ error: "Incorrect old password" });
+        }
+
+        const salt2 = crypto.randomBytes(16);
+        const newHashedPassword = await pbkdf2Async(
+            newPassword,
+            salt2,
+            310000,
+            32,
+            "sha256"
+        );
+
+        await possibleUser.update({
+            passwordHash: newHashedPassword,
+            passwordSalt: salt,
+        });
+
+        res.status(200).json({
+            success: true,
+            message: "Password changed successfully",
+        });
+    } catch (error) {
+        next(error);
     }
-
-    request.isApproved = false;
-    request.isReviewed = true;
-    await request.save();
-
-    res.status(StatusCodes.OK).json({
-      message: "Request rejected successfully",
-    });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-};
-
-const getProfileVerificationRequestList = async (req, res, next) => {
-  try {
-    let filter = {};
-    const { status } = req.query;
-
-    if (status === "approved") {
-      filter = { isApproved: true };
-    } else if (status === "rejected") {
-      filter = { isApproved: false, isReviewed: true };
-    } else if (status === "pending") {
-      filter = { isReviewed: false };
-    } else {
-      filter = {};
-    }
-
-    const requests = await db.ProfileVerification.findAll({
-      where: filter,
-      include: [
-        {
-          model: db.User,
-          as: "creator",
-          attributes: ["firstName", "lastName", "email"],
-        },
-      ],
-    });
-    res.status(StatusCodes.OK).json(requests);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
 };
 
 module.exports = {
-  createUser,
-  getUserList,
-  deleteUser,
-  profileVerificationRequest,
-  getProfileVerificationRequest,
-  approveProfileVerificationRequest,
-  rejectProfileVerificationRequest,
-  getProfileVerificationRequestList,
-  getProfileVerificationRequestByUser,
+    createUser,
+    getUserList,
+    deleteUser,
+    profileVerificationRequest,
+    getProfileVerificationRequest,
+    approveProfileVerificationRequest,
+    rejectProfileVerificationRequest,
+    getProfileVerificationRequestList,
+    getProfileVerificationRequestByUser,
+    changePassword,
+    updateUser,
+    getUserInfo,
 };
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const {
-  createUser,
-  getUserList,
-  deleteUser,
-  profileVerificationRequest,
-  getProfileVerificationRequestList,
-  getProfileVerificationRequest,
-  approveProfileVerificationRequest,
-  rejectProfileVerificationRequest,
-  getProfileVerificationRequestByUser,
+    createUser,
+    getUserList,
+    deleteUser,
+    profileVerificationRequest,
+    getProfileVerificationRequestList,
+    getProfileVerificationRequest,
+    approveProfileVerificationRequest,
+    rejectProfileVerificationRequest,
+    getProfileVerificationRequestByUser,
+    getUserInfo,
+    updateUser,
+    changePassword,
 } = require("../controllers/user");
 
 const upload = require("../middleware/upload");
@@ -18,25 +21,31 @@ const router = express.Router();
 router.post("/", createUser);
 router.get("/", getUserList);
 router.delete("/:id", deleteUser);
+router.get("/:id", getUserInfo);
 
 router.post(
-  "/profile-verification",
-  upload.fields([
-    { name: "governmentId", maxCount: 1 },
-    { name: "proofDocument", maxCount: 1 },
-  ]),
-  profileVerificationRequest
+    "/profile-verification",
+    upload.fields([
+        { name: "governmentId", maxCount: 1 },
+        { name: "proofDocument", maxCount: 1 },
+    ]),
+    profileVerificationRequest
 );
 router.get("/profile-verification", getProfileVerificationRequestList);
 router.get("/profile-verification/me", getProfileVerificationRequestByUser);
 router.get("/profile-verification/:id", getProfileVerificationRequest);
 router.put(
-  "/profile-verification/:id/approve",
-  approveProfileVerificationRequest
+    "/profile-verification/:id/approve",
+    approveProfileVerificationRequest
 );
 router.put(
-  "/profile-verification/:id/reject",
-  rejectProfileVerificationRequest
+    "/profile-verification/:id/reject",
+    rejectProfileVerificationRequest
 );
+
+// updates
+router.put("/:id", upload.single("profilePicture"), updateUser);
+// router.put("/:id", updateUser);
+router.post("/change-password/:id", changePassword);
 
 module.exports = router;
